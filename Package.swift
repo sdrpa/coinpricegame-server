afbec072605aba5e0d5aa0273ca4e50e112be043
Sinisa Drpa <sdrpa@tagtaxa.com>
2018-01-17 08:30:48 +0000

    Remove KituraCORS local module, use the version from GithHub

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
    name: "App",
    dependencies: [
       .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.1.0"),
+      .package(url: "https://github.com/sdrpa/Kitura-CORS.git", from: "2.0.1"),
       .package(url: "https://github.com/IBM-Swift/Kitura-WebSocket", from: "1.0.0"),
       .package(url: "https://github.com/IBM-Swift/Kitura-Cache", from: "2.0.0"),
       .package(url: "https://github.com/IBM-Swift/Swift-Kuery-PostgreSQL", from: "1.0.0"),
@@ -14,9 +15,6 @@ let package = Package(
       .package(url: "https://github.com/IBM-Swift/SwiftyJSON", from: "17.0.0")
    ],
    targets: [
-      .target(
-         name: "KituraCORS",
-         dependencies: ["Kitura"]),
       .target(
          name: "Then",
          dependencies: []),
